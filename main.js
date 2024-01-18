2b01670479b6dfaad1bd3f44bea5910c95cc857e
matheusrodacki <matheu.rodacki@outlook.com>
2024-01-18 12:10:17 -0300

    feat:

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaDeItens = [];
+let itemAEditar;
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
@@ -45,20 +46,24 @@ function mostrarItem() {
         <div>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
-    </li>
-        `;
+    </li>`;
     } else {
       ulItens.innerHTML += `
     <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+            <input type="text" class="is-size-5" value="${elemento.valor}" 
+            ${index !== Number(itemAEditar) ? "disabled" : ""}></input>
         </div>
         <div>
+            ${
+              index === Number(itemAEditar)
+                ? "<button onclick='salvarEdicao()'><i class='fa-regular fa-floppy-disk is-clickable'></i></button>"
+                : "<i class='fa-regular is-clickable fa-pen-to-square editar'></i>"
+            }
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
-    </li>
-    `;
+    </li>`;
     }
   });
 
@@ -83,4 +88,25 @@ function mostrarItem() {
       mostrarItem();
     });
   });
+
+  const editarItens = document.querySelectorAll(".editar");
+  editarItens.forEach((i) => {
+    i.addEventListener("click", (evento) => {
+      itemAEditar =
+        evento.target.parentElement.parentElement.getAttribute("data-value");
+
+      mostrarItem();
+      console.log(itemAEditar);
+    });
+  });
+}
+
+function salvarEdicao() {
+  const itemEditado = document.querySelector(
+    `[data-value="${itemAEditar}"] input[type="text"]`
+  );
+  listaDeItens[itemAEditar].valor = itemEditado.value;
+  console.log(listaDeItens);
+  itemAEditar = -1;
+  mostrarItem();
 }
